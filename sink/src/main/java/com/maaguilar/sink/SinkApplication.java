c4359c17cdf2af2c434ce61bc383980ab10ac1ee
Miguel Ángel Aguilar <miaguicam@gmail.es>
2020-02-16 22:24:41 +0100

    added reactive pipelines example

---
+++ sink/src/main/java/com/maaguilar/sink/SinkApplication.java
@@ -0,0 +1,40 @@
+package com.maaguilar.sink;
+
+import lombok.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Consumer;
+
+@SpringBootApplication
+public class SinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinkApplication.class, args);
+	}
+
+}
+
+@EnableBinding(Sink.class)
+class Attendant {
+	@Bean
+	Consumer<Flux<FlyingPassenger>> logPassengers() {
+		return flux -> flux.subscribe(System.out::println);
+	}
+}
+
+@Value
+class FlyingPassenger {
+	enum Status {
+		VALUED,
+		PREMIUM,
+		ADDITIONAL_SECURITY_SCREENING_SEAT_BY_TOILETS
+	}
+
+	private final String id, name;
+	private final Status status;
+}
---
+++ source/src/main/java/com/maaguilar/source/SourceApplication.java
@@ -0,0 +1,55 @@
+package com.maaguilar.source;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class SourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceApplication.class, args);
+	}
+
+}
+
+@EnableBinding(Source.class)
+@AllArgsConstructor
+class DeskAgent {
+	private final PassengerGenerator generator;
+
+	@Bean
+	Supplier<Flux<Passenger>> sendPassengerToGate(){
+		return () -> Flux.interval(Duration.ofSeconds(1))
+				.onBackpressureDrop()
+				.map(l -> generator.generate());
+	}
+}
+
+@Component
+class PassengerGenerator {
+	private List<String> names = Arrays.asList("Artem", "Haytham", "Elsa", "Shellee", "Gowran", "Mark");
+	private Random rnd = new Random();
+
+	Passenger generate (){
+		return new Passenger(UUID.randomUUID().toString(), names.get(rnd.nextInt(names.size())));
+	}
+}
+
+@Value
+class Passenger {
+	private final String id, name;
+}
